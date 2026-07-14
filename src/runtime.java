ea002f3923127429976dfa251eeb0af3c53d643d
chichi <uhae451@gmail.com>
2026-07-14 16:22:28 +0900

    log n

--- src/runtime.java
+++ src/runtime.java
@@ -42,5 +42,18 @@ public class runtime {
             System.out.println("hi2");
         }
         System.out.println("bye");
+
+
+        // O(log n)
+        // 아래코드는 n이 1000이라면 while문에 조건은 배가 되기 때문에
+        // 1 2 4 8 16 32 64 128 256 512 -> 10번 반복된다.
+        // n이 들어와도 실제로 안에 while문은 한참 적은 수만큼 반복이 되는것이다.
+        // 이때 기하급수적으로 주는 공식으로 log를 취하면 된다.
+        for(int i = 0; i < n; i++){
+            int j = 1;
+            while(j < n ){
+                j *= 2;
+            }
+        }
     }
 }
