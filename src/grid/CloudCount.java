dc8363110970554f3c707d5d58e93619c35e7c73
chichi <uhae451@gmail.com>
2026-07-26 19:07:55 +0900

    구름갯수세기

--- src/grid/CloudCount.java
+++ src/grid/CloudCount.java
@@ -1,4 +1,69 @@
 package grid;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class CloudCount {
+    public static void main(String[] args) {
+
+        /*
+        * 그리가 주어졌을때 1이 구름이고 상,하,좌,우 연결된 구름은 하나의 구름으로 볼때 구름의 총 갯수 구하기
+        * */
+        int[][] grid = new int[][]{};
+        grid = new int[][]{
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 1}
+        };
+        System.out.println(bfs(grid));
+    }
+
+    static int solution(int[][]grid){
+        int n = grid.length;
+        int m = grid[0].length;
+
+        return 0;
+    }
+
+
+    static int bfs(int[][] grid){
+        Deque<int[]> que = new ArrayDeque<>();
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+        int[] dr = {1,0,-1,0};
+        int[] dc = {0,1,0,-1};
+        boolean[][] visited = new boolean[n][m];
+        que.offer(new int[]{0,0});
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!visited[i][j] && grid[i][j] == 1){
+                    visited[i][j] = true;
+                    que.offer(new int[]{i,j});
+                    while (!que.isEmpty()) {
+                        int[] now = que.poll();
+                        int r = now[0];
+                        int c = now[1];
+                        for(int q=0; q<4; q++){
+                            int nr = r + dr[q];
+                            int nc = c + dc[q];
+                            if(nr >= 0 && nr < n && nc >=0 && nc < m){
+                                if(!visited[nr][nc] && grid[nr][nc] == 1 ){
+                                    visited[nr][nc] = true;
+                                    que.offer(new int[]{nr,nc});
+                                }
+                            }
+                        }
+                    }
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+
+
 }
